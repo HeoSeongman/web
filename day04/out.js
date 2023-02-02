cd6baa5f313316700dd6d0a45b03019cc0e934da
HeoSeongman <tjdaks0804@gmail.com>
2023-02-02 17:45:45 +0900

    2023/02/02

---
+++ day04/out.js
@@ -0,0 +1,21 @@
+function wash() {
+  console.log("1. 먼지를 털자.")
+  console.log("2. 물에 집어 넣는다.")
+  console.log("3. 비누를 뭍힌다..")
+  console.log("4. 오물조물 비빈다.")
+  console.log("5. 물에 여러번 헹군다.")
+  console.log("6. 물을 짠다.")
+  console.log("7. 빨랫대에 널어둔다.")
+}
+
+function clean() {
+  document.writeln("1. 청소기를 집어든다.<br>");
+  document.writeln("2. 전원버튼을 눌러 작동시킨다.<br>");
+  document.writeln("3. 청소기를 앞뒤좌우 밀고 당기며 제대로된 청소를 시작한다.<br>");
+  document.writeln("4. 먼지같이 작은 입자는 빨아들인다.<br>");
+  document.writeln("5. 큰 입자는 손으로 집어 쓰레기통이 집어 넣는다.<br>");
+  document.writeln("6. 다 되었으면 청소기를 제자리에 갖다놓는다.<br>");
+  document.writeln("7. 청소가 잘 된 방을 보고 뿌듯해한다.<br>");
+  document.writeln("8. 살짝 땀이 났다.<br>");
+  document.writeln("9. 샤워를 한다.<br>");
+}
---
+++ day04/out2.js
@@ -0,0 +1,23 @@
+function travel() {
+  console.log("여행지에 도착을 했다!");
+  console.log("여긴 바닷가. 바닷바람이 찹다.");
+  console.log("하지만 지루한 일상에서 벗어나 맞는 찬바람은 상쾌하다.");
+  console.log("조금은 짭다.");
+  console.log("해변가를 걷는다.");
+  console.log("사람을 별로 없지만 파도가 나를 심심하지 않게 출렁인다.");
+  console.log("더이상 여기서 할게 없다...");
+}
+
+function rest() {
+  console.log("오늘 수업은 끝났다.");
+  console.log("열심히 이해하려고 노력한 나, 꽤 대단할지도?.");
+  console.log("머리가 살짝 지끈거린다. 바람을 쐬러 밖에 나가보자.");
+  console.log("이어폰을 꺼내 귀에 꽂는다.");
+  console.log("요즘 유행하는 노래를 틀어본다.");
+  console.log("뭐라하는지 도통 알아들을 수가 없다. 듣던거나 듣자.");
+  console.log("아주 센 바람이 나를 치고 지나간다. 숨도 잘 못 쉬겠고, 귀가 찢어질 것만 같다.");
+  console.log("바람을 피해 집에 뛰어 들어온다.");
+  console.log("바로 전기장판을 켜고 드러눕고 이불을 덮는다.");
+  console.log("점점 따듯해져 몸이 풀이진다.");
+  console.log("이게 쉬는거지~ 이불 밖은 위험해~");
+}
